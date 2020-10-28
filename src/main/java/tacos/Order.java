8b9b5b20d98cc511f7e306955bc6a6fcbbcc3faf
inwoo <inwoox@naver.com>
2020-10-28 17:15:50 +0900

    유효성 검사 추가

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,14 +1,34 @@
 package tacos;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
 public class Order {
+	
+	@NotBlank(message="Name is Required")
 	private String deliveryName;
+	@NotBlank(message="Street is Required")
 	private String deliveryStreet;
+	@NotBlank(message="City is Required")
 	private String deliveryCity;
+	@NotBlank(message="State is Required")
 	private String deliveryState;
+	@NotBlank(message="Zip is Required")
 	private String deliveryZip;
+	@CreditCardNumber(message="Not a valid Credit Card Number")
 	private String ccNumber;
+	@Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message="Must be formatted MM/YY")
 	private String ccExpiration;
+	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
+	
+	
+	
+	
+	
 	
 	public String getDeliveryName() {
 		return deliveryName;
