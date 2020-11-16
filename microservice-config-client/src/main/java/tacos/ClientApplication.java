15030902027727259cda1d80da126c722b5db994
inwoo <inwoox@naver.com>
2020-11-16 18:49:25 +0900

    애플리케이션이나 프로파일에 특정된 속성 (예를 들어 Git Repo에 올라가있는 공통 속성인 application.yml보다 우선되는 application-dev.yml 등)

--- microservice-config-client/src/main/java/tacos/ClientApplication.java
+++ microservice-config-client/src/main/java/tacos/ClientApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-// spring-cloud-starter-config 의존성을 추가하고, 이 애플리케이션을 실행하면, 
-// 자동 구성이 실행되어 , 구성 서버로부터 속성들을 가져온다.
+// spring-cloud-starter-config 의존성을 추가하고, 이 클라이언트 애플리케이션을 실행하면, 
+// 자동 구성이 실행되어 , 구성 서버로부터 속성들을 가져온다. (구성 서버는 Git 리포지토리에서 속성을 가져오는 것이다)
 // 구성 서버의 위치는 application.yml에 설정한다.
 
 
